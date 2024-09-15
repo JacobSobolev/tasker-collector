c49f7f19092fdecfc1f0151d10634a6939e401bd
Jacob Sobolev <yakovsobolev1@gmail.com>
2024-09-15 11:32:05 +0300

    add getting task name param and error handle

--- app/tasker/routes.py
+++ app/tasker/routes.py
@@ -1,10 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 tasker_bp = Blueprint("tasker",__name__, url_prefix="/tasker")
 
+TASK_TYPES = {"sum": 2, "multiply": 3}
+
+def get_json_task():
+    try:
+        task = request.json["task"]
+    except KeyError:
+        return {"error": "no task provided"}
+    if task not in TASK_TYPES:
+        return {"error": "task not supported"}
+    return task
+
 @tasker_bp.route("/", methods=["POST"])
 def do_task():
-    return "do task"
+    task = get_json_task()
+    if "error" in task:
+        return task, 400
+    return "ok"
 
 @tasker_bp.route("/")
 def show_tasks():
