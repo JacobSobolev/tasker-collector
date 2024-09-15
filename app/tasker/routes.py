1a769390771701a1f050789def0e19b8fbbdf2c9
Jacob Sobolev <yakovsobolev1@gmail.com>
2024-09-15 11:41:02 +0300

    add getting tasker params and error handling

--- app/tasker/routes.py
+++ app/tasker/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from ast import literal_eval
 
 tasker_bp = Blueprint("tasker",__name__, url_prefix="/tasker")
 
@@ -13,11 +14,32 @@ def get_json_task():
         return {"error": "task not supported"}
     return task
 
+def get_json_params(task):
+    try:
+        params = literal_eval(request.json["params"])
+        # params = request.json["params"]
+    except KeyError:
+        return {"error": "no params provided"}
+    except ValueError:
+        return {"error": "params needs to be a list"}
+    if not isinstance(params, list):
+        return {"error": "params needs to be a list"}
+    if not all(isinstance(item, int) for item in params):
+        return {"error": "list needs to be only int"}
+    if not TASK_TYPES[task] == len(params):
+        return {"error": "list not in the correct size"}
+    return params
+
 @tasker_bp.route("/", methods=["POST"])
 def do_task():
     task = get_json_task()
     if "error" in task:
         return task, 400
+    params = get_json_params(task)
+    if "error" in params:
+        return params, 400
+
+
     return "ok"
 
 @tasker_bp.route("/")
